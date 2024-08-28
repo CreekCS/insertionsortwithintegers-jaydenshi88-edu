72acbca34a1d74d1633b31768c1a1bcc5c42f62a
jaydeshi88 <jaydenshi415@gmail.com>
2024-08-28 16:10:52 -0600

    finsih

--- InsSortInt.java
+++ InsSortInt.java
@@ -1,3 +1,38 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class InsSortInt {
 
+    public static void main(String[] args) {
+        ArrayList<Integer> numbers = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader("randInts.txt"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                int number = Integer.parseInt(line);
+                numbers.add(number);
+            }
+        } catch (IOException e) {
+            System.err.println("Error reading file: " + e.getMessage());
+        } catch (NumberFormatException e) {
+            System.err.println("Error converting line to number: " + e.getMessage());
+        }
+        insertionSort(numbers);
+        System.out.println(numbers.get(0));
+        System.out.println(numbers.get(numbers.size() - 1));
+    }
+
+    public static void insertionSort(ArrayList<Integer> list) {
+        for (int i = 1; i < list.size(); i++) {
+            int key = list.get(i);
+            int j = i - 1;
+            while (j >= 0 && list.get(j) > key) {
+                list.set(j + 1, list.get(j));
+                j = j - 1;
+            }
+            list.set(j + 1, key);
+        }
+    }
 }
